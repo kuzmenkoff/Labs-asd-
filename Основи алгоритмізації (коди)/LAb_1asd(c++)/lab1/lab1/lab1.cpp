5163b6a547c4b3fc930c800c335a29530b015fc5
kuzmenkoff <47691438+kuzmenkoff@users.noreply.github.com>
2021-11-21 13:52:51 +0200

    Add files via upload

---
+++ Основи алгоритмізації (коди)/LAb_1asd(c++)/lab1/lab1/lab1.cpp
@@ -0,0 +1,23 @@
+﻿
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	std::cout << "Ведите значение температуры по Цельсию." << endl;
+	long double c, f, k;
+	std::cin >> c; 
+	f = c * 1.8 + 32; 
+	k = c + 273.15;
+	std::cout << "°F=" << f << endl;
+	std::cout << "°K=" << k << endl;
+
+
+
+}
+
+
